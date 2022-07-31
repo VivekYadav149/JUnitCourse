13f27af1f00a3956680ec38d6b294490f493d70c
Vivek Yadav <vivek.yadav@adidas.com>
2022-07-31 09:20:28 +0530

    Refactoring First JUnit Test

--- src/test/java/project/junit/helper/StringHelperTest.java
+++ src/test/java/project/junit/helper/StringHelperTest.java
@@ -6,13 +6,28 @@ import org.junit.Test;
 
 public class StringHelperTest {
 
+	// AACD => CD ACD => CD CDEF=>CDEF CDAA => CDAA
+	
+	StringHelper helper = new StringHelper();
+
 	@Test
-	public void test() {
-		StringHelper helper = new StringHelper();
-		// AACD => CD ACD => CD CDEF=>CDEF CDAA => CDAA
-
+	public void testTruncateAInFirst2Positions_AinFirst2Positions() {
 		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
-		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
-
 	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_AinFirstPosition() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_DoesntContainA() {
+		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_AinLast2Positions() {
+		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+	}
+	
 }
