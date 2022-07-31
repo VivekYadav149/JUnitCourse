89799691c8dd3e431df4565e9dbc2b8659f2878e
Vivek Yadav <vivek.yadav@adidas.com>
2022-07-31 10:26:41 +0530

    Testing Exceptions in JUnit

--- src/test/java/project/junit/helper/ArraysCompareTest.java
+++ src/test/java/project/junit/helper/ArraysCompareTest.java
@@ -2,13 +2,34 @@ package project.junit.helper;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class ArraysCompareTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testArraySort_RandomArray() {
+		int[] numbers = { 12, 3, 4, 1 };
+		int[] expected = { 1, 3, 4, 12 };
+		Arrays.sort(numbers);
+		assertArrayEquals(expected, numbers);
 	}
 
+	// Testing Exceptions 
+//	@Test()
+//	public void testArraySort_NullArray() {
+//		int[] numbers = null;
+//		try {
+//		Arrays.sort(numbers);
+//		} catch (NullPointerException e) {	
+//		}
+//	}  
+	
+	@Test(expected=NullPointerException.class)
+	public void testArraySort_NullArray() {
+		int[] numbers = null;
+		Arrays.sort(numbers);
+	}
+	
 }
