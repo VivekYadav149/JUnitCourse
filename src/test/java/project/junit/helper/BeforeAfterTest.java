5828e1d64a12363b389233ec2e14d31b05fc6de1
Vivek Yadav <vivek.yadav@adidas.com>
2022-07-31 10:10:50 +0530

    Implementing Before After

---
+++ src/test/java/project/junit/helper/BeforeAfterTest.java
@@ -0,0 +1,29 @@
+package project.junit.helper;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BeforeAfterTest {
+	
+	@Before
+	public void setup(){
+		System.out.println("Before Test");
+	}
+
+	@Test
+	public void test1() {
+		System.out.println("test1 executed");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("test2 executed");
+	}
+
+	@After
+	public void teardown() {
+		System.out.println("After test");
+	}
+	
+}
